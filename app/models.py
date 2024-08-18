354a2ef6c4e697efa9cf5e2610ef16711c7d60a7
antobarbero <antobarbero94@gmail.com>
2024-08-18 11:37:58 +0200

    Implement crud endpoints for recipes

--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-from typing import List, Optional
+from typing import Optional
 from uuid import uuid4
 from pydantic import BaseModel, Field
 
@@ -7,16 +7,16 @@ class Recipe(BaseModel):
     id: str = Field(default_factory=uuid4, alias="_id")
     title: str
     instructions: str
-    ingredients: List[str]
+    ingredients: list[str]
 
     class Config:
-        allow_population_by_field_name = True
-        schema_extra = {
+        populate_by_name = True
+        json_schema_extra = {
             "example": {
                 "_id": "066de609-b04a-4b30-b46c-32537c7f1f6e",
                 "title": "Chicken with onions",
                 "instructions": "Cook the onions with the chicken.",
-                "ingredients": "['onions', 'chicken']",
+                "ingredients": ["onions", "chicken"],
             }
         }
 
@@ -24,11 +24,10 @@ class Recipe(BaseModel):
 class RecipeUpdate(BaseModel):
     title: Optional[str]
     instructions: Optional[str]
-    ingredients: Optional[List[str]]
+    ingredients: Optional[list[str]]
 
     class Config:
-        allow_population_by_field_name = True
-        schema_extra = {
+        json_schema_extra = {
             "example": {
                 "title": "Chicken with onions",
                 "instructions": "Cook the onions with the chicken.",
